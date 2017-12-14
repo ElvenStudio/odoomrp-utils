c851764a3a746ef4e444efb72e83e80a9ce09b18
Domenico Stragapede <d.stragapede@elvenstudio.it>
2017-12-14 22:00:19 +0100

    [FIX] stock_quant_reservation_status
     * avoid field reserved_for computation when is needed the is_reserved field.

--- stock_quant_reservation_status/models/stock.py
+++ stock_quant_reservation_status/models/stock.py
@@ -3,16 +3,26 @@
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
 
-from openerp import models, fields, api
+from openerp import models, fields, api, _
 
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
+    is_reserved = fields.Boolean(
+        string=_('Reserved Quant'),
+        compute='_is_reserved',
+        store=True
+    )
+
+    reserved_for = fields.Char(
+        string=_('Reserved for'),
+        compute='_reserved_for'
+    )
+
     @api.one
     @api.depends('reservation_id')
-    def _is_reserved(self):
-        self.is_reserved = bool(self.reservation_id)
+    def _reserved_for(self):
         reserved_for = ''
         if self.reservation_id:
             partner_name = self.reservation_id.picking_id.partner_id.name or ''
@@ -27,10 +37,10 @@ class StockQuant(models.Model):
 
         self.reserved_for = reserved_for
 
-    is_reserved = fields.Boolean(
-        string='Reserved Quant', compute='_is_reserved', store=True)
-    reserved_for = fields.Char(
-        string='Reserved for', compute='_is_reserved')
+    @api.one
+    @api.depends('reservation_id')
+    def _is_reserved(self):
+        self.is_reserved = bool(self.reservation_id)
 
     @api.multi
     def action_view_related_picking(self):
