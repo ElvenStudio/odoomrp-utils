d0ffcedaf4cd528551c64e2cc0658d1b89abb752
Domenico Stragapede <d.stragapede@elvenstudio.it>
2019-03-27 12:47:16 +0100

    [8.0][IMP] stock_warehouse_orderpoint_stock_info   * added incoming, outgoing and virtual quantity on orderpoint

--- stock_warehouse_orderpoint_stock_info/models/stock_warehouse_orderpoint.py
+++ stock_warehouse_orderpoint_stock_info/models/stock_warehouse_orderpoint.py
@@ -11,17 +11,32 @@ class StockWarehouseOrderpoint(models.Model):
 
     @api.one
     def _product_available_qty(self):
-        self.product_location_qty = self.product_id.with_context(
-            location=self.location_id.id
-            )._product_available()[self.product_id.id]['qty_available']
+        product = self.product_id \
+            .with_context(location=self.location_id.id) \
+            ._product_available()[self.product_id.id]
+        self.product_location_qty = product['qty_available']
+        self.product_location_qty_virtual = product['virtual_available']
+        self.product_location_qty_incoming = product['incoming_qty']
+        self.product_location_qty_outgoing = product['outgoing_qty']
 
     @api.one
     @api.depends('product_location_qty', 'product_min_qty')
     def _product_available(self):
+        # TODO review
         self.available = self.product_location_qty > self.product_min_qty
 
     product_location_qty = fields.Float(
         string='Quantity On Location', compute='_product_available_qty')
+
+    product_location_qty_virtual = fields.Float(
+        string='Virtual Quantity On Location', compute='_product_available_qty')
+
+    product_location_qty_incoming = fields.Float(
+        string='Incoming Quantity On Location', compute='_product_available_qty')
+
+    product_location_qty_outgoing = fields.Float(
+        string='Outgoing Quantity On Location', compute='_product_available_qty')
+
     available = fields.Boolean(
         string='Is enough product available?', compute='_product_available',
         store=True)
